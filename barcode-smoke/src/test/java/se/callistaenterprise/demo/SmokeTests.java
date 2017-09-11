80124ef1a1b7fbe26509412a5fbb351e46e2ff19
andtell <andreas.tell@callistaenterprise.se>
2017-09-11 11:45:19 +0200

    Added context path. Updated to Spring Boot 2.M3

--- barcode-smoke/src/test/java/se/callistaenterprise/demo/SmokeTests.java
+++ barcode-smoke/src/test/java/se/callistaenterprise/demo/SmokeTests.java
@@ -1,36 +1,38 @@
 package se.callistaenterprise.demo;
 
 
-import io.restassured.RestAssured;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.test.context.junit4.SpringRunner;
 import se.callistaenterprise.SmokeTestSupport;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.is;
 
 @Slf4j
-public class SmokeTests extends SmokeTestSupport  {
+public class SmokeTests extends SmokeTestSupport {
 
-	@Test
-	public void canAccessGoogle() {
-		given().when().get("http://www.google.com:80").then().statusCode(200);
-	}
+    private static final String CONTEXT_PATH = "/breakfast";
 
-	@Test
-	public void appIsUp() {
-		get("/admin/health");
-	}
+    @Test
+    public void canAccessGoogle() {
+        given().when().get("http://www.google.com:80").then().statusCode(200);
+    }
 
-	@Test
-	public void greetingsWork() {
-		get("/greetings?name=test")
-				.body("message", is("Howdy test !"));
-	}
+    @Test
+    public void appIsUp() {
+        get(CONTEXT_PATH + "/admin/health");
+    }
+
+    @Test
+    public void barcodesWork() {
+        given().when().get(CONTEXT_PATH + "/barcodes").then().statusCode(200);
+    }
+
+    @Test
+    public void greetingsWork() {
+        get(CONTEXT_PATH + "/greetings?name=test")
+                .body("message", is("Howdy test !"));
+    }
 
 }
 
